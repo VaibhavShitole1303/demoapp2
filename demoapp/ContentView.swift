1ea16110a6bfcb52ed2edddaf1eb45f99f41d9f9
neosaoservices <neosaoservices@gmail.com>
2024-03-25 16:49:14 +0530

    Initial Commit

---
+++ demoapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demoapp
+//
+//  Created by Neosao Services on 25/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demoapp/demoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoappApp.swift
+//  demoapp
+//
+//  Created by Neosao Services on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct demoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
